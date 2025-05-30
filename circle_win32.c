4ddf4eab3a03d0faba205b638ce096edd4adaab1
Steve Schnepp <steve.schnepp@pwkf.org>
2025-05-31 00:07:35 +0200

    add hello world text

--- circle_win32.c
+++ circle_win32.c
@@ -17,9 +17,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
-void draw_circle(float x, float y, float r, float width) {
+void draw_circle(float x, float y, float r, float width, float r_col, float g_col, float b_col, float alpha) {
     float r0 = r;
     float r1 = r + width;
+
+    glColor4f(r_col, g_col, b_col, alpha);
 
     glBegin(GL_TRIANGLE_STRIP);
     for (int i = 0; i <= CIRCLE_POINTS; i++) {
@@ -31,6 +33,15 @@ void draw_circle(float x, float y, float r, float width) {
         glVertex2f(x + cx * r1, y + cy * r1);
     }
     glEnd();
+}
+
+void draw_text(float x, float y, const char *text) {
+    glColor4f(1, 1, 1, 1); // white
+    glRasterPos2f(x, y);
+    glPushAttrib(GL_LIST_BIT);
+    glListBase(1000);
+    glCallLists((GLsizei)strlen(text), GL_UNSIGNED_BYTE, text);
+    glPopAttrib();
 }
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow) {
@@ -62,7 +73,20 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow) {
     int pf = ChoosePixelFormat(hDC, &pfd);
     SetPixelFormat(hDC, pf, &pfd);
     HGLRC rc = wglCreateContext(hDC);
+
+    HFONT hFont = CreateFontA(
+        -24, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
+        ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
+        ANTIALIASED_QUALITY, FF_DONTCARE | DEFAULT_PITCH,
+        "Courier New");
+
+    SelectObject(hDC, hFont);
+    wglUseFontBitmapsA(hDC, 0, 256, 1000); // base list ID = 1000
+
     wglMakeCurrent(hDC, rc);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     ShowWindow(hWnd, SW_SHOW);
 
@@ -82,7 +106,8 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow) {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glLoadIdentity();
-        draw_circle(pos_x, 0.0f, 0.1f, 0.02f);
+        draw_circle(pos_x, 0.0f, 0.1f, 0.03f, 1.0f, 0.0f, 0.0f, 0.5f); // semi-transparent red
+        draw_text(-0.9f, 0.8f, "hello world");
 
         SwapBuffers(hDC);
         Sleep(16);
